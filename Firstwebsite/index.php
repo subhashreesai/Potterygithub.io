590e8a9f18c26fafe91d078f18b1205631eaf06e
subhashreesai <49429650+subhashreesai@users.noreply.github.com>
2020-10-25 12:35:20 +0530

    My pottery website

---
+++ Firstwebsite/index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+  
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	 <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
+	 <link rel="stylesheet" type="text/css" href="css/style.css">
+	 
+</head>
+<body>
+
+	<!---Navbar---->
+<nav class="navbar navbar-expand-lg navbar-dark bg-warning p-3">
+  <a class="navbar-brand" href="#"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav m-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#service">Services</a>
+      
+       </li>
+       <li class="nav-item active">
+        <a class="nav-link" href="#about">About</a>
+      
+       </li>
+       <li class="nav-item active ">
+        <a class="nav-link" href="#contact">Contact</a>
+      
+       </li>
+    </ul>
+  </div>
+</nav>
+
+ <!-- The slideshow----->
+ <div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/image4.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/image6.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/image7.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+<!----about us---->
+<div id="about">
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">About Us</h2>
+		
+	</div>
+	<div class="container-fluid">
+	<div class="row">
+	<div class="col-lg-6 col-md-6 col-12">
+		<img src="images/image13.jpg" class="img-fluid ">
+	</div>
+	<div class="col-lg-6 col-md-6 col-12">
+		<h2 class="display-4">About More</h2>
+		<p class="py-3">Lorem ipsum doller than man so in that asa book ditoor.Lorem ipsum doller than man so i.Lorem ipsum doller than man so i.Lorem ipsum doller than man so i.Lorem ipsum doller than man so i.Lorem ipsum doller than man so i.Lorem ipsum doller than man so i</p>
+		<a href="about.php" class="btn btn-success">check more</a>
+	</div>	
+	</div>
+</section>
+</div>
+
+     <!----services---->
+     <div id="service">
+     <section class="my-5">
+     	<div class="py-5">
+		<h2 class="text-center">Our Services</h2>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row">
+	<div class="col-lg-4 col-md-4 col-12">
+  <div class="card" style="width:400px">
+  <img class="card-img-top" src="images/image14.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Price 10$</h4>
+  </div>
+</div>
+</div>
+
+
+
+	<div class="col-lg-4 col-md-4 col-12">
+  <div class="card" style="width:400px">
+  <img class="card-img-top" src="images/image14.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Price 10$</h4>
+  </div>
+</div>
+</div>
+
+
+	<div class="col-lg-4 col-md-4 col-12">
+  <div class="card" style="width:400px">
+  <img class="card-img-top" src="images/image14.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Price 10$</h4>
+  </div>
+</div>
+</div>
+</div>
+</div>
+
+     </section>
+ </div>
+
+     <!----Gallery---->
+     <div id="gallery">
+     <section class="my-5">
+     	<div class="py-5">
+     		<h2 class="text-center">Our gallery</h2>
+     		
+     	</div>
+     	<div class="container-fluid">
+     		<div class="row">
+     			<div class="col-lg-4 col-md-4 col-12">
+     				<img src="images/image10.jpg" class="img-fluid pb-4">
+     			</div>
+     			
+     			<div class="col-lg-4 col-md-4 col-12">
+     				<img src="images/image10.jpg" class="img-fluid pb-4">
+     			</div>
+     			<div class="col-lg-4 col-md-4 col-12">
+     				<img src="images/image10.jpg" class="img-fluid pb-4">
+     			</div>
+     			<div class="col-lg-4 col-md-4 col-12">
+     				<img src="images/image10.jpg" class="img-fluid pb-4">
+     			</div>
+     			<div class="col-lg-4 col-md-4 col-12">
+     				<img src="images/image10.jpg" class="img-fluid pb-4">
+     			</div>
+     			<div class="col-lg-4 col-md-4 col-12">
+     				<img src="images/image10.jpg" class="img-fluid pb-4">
+     			</div>
+     			<div class="col-lg-4 col-md-4 col-12">
+     				<img src="images/image10.jpg" class="img-fluid pb-4">
+     			</div>
+     			<div class="col-lg-4 col-md-4 col-12">
+     				<img src="images/image10.jpg" class="img-fluid pb-4">
+     			</div>
+     			<div class="col-lg-4 col-md-4 col-12">
+     				<img src="images/image10.jpg" class="img-fluid pb-4">
+     			</div>
+     			
+     		</div>
+     		
+     	</div>
+     	
+     </section>
+ </div>
+
+     <!----contact form---->
+     <div id="contact">
+     <section class="my-5">
+     	<div class="py-5">
+     		<h2 class="text-center">Contact us</h2>
+     		
+     	</div>
+     	<div class="w-50 m-auto">
+     		<form action="userinfo.php" method="post">
+     			<div class="form_group">
+     				<label>Username</label>
+     				<input type="text" name="user" autocomplete="off" class="form-control">
+     			</div>
+     			<div class="form_group">
+     				<label>Email Id</label>
+     				<input type="text" name="email" autocomplete="off" class="form-control">
+     			</div>
+     			<div class="form_group">
+     				<label>Mobile</label>
+     				<input type="text" name="mobile" autocomplete="off" class="form-control">
+     			</div>
+     			<div class="form_group">
+     				<label>Comments</label>
+     				<textarea class="form-control" name="commemt">
+     					
+     				</textarea>
+     				</div>
+     				<br>
+     				
+     				<button type="submit" class="btn btn-warning">Submit</button>
+     			
+     		</form>
+     	</div>
+
+     	
+     </section>
+     </div>
+
+
+
+     <!---footer---->
+     <footer>
+     	<h4 class="p-3 bg-warning text-white text-center">@subhashree</h4>
+     </footer>
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
